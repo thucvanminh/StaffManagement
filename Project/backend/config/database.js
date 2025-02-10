2f9106b73700fb5ecfdcf6cb4c94ad651ea6ceef
thucvanminh <thucnichan2929vmt@gmail.com>
2025-02-10 11:15:13 +0700

    test

--- Project/backend/config/database.js
+++ Project/backend/config/database.js
@@ -1,9 +1,11 @@
 // backend/config/database.js
+// Load biến môi trường từ file .env
+require('dotenv').config();
 
 const { Sequelize } = require('sequelize');
 
 // Khởi tạo Sequelize
-const sequelize = new Sequelize('staffmanagement', 'root', '123456', {
+const sequelize = new Sequelize('staffmanagement', 'root', process.env.DB_PASSWORD, {
   host: 'localhost',
   dialect: 'mysql',
 });
