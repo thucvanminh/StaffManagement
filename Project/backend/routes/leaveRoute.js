3f88404de2d778446684481af7607265353c9181
thucvanminh <thucnichan2929vmt@gmail.com>
2025-03-19 13:06:47 +0700

    .

--- Project/backend/routes/leaveRoute.js
+++ Project/backend/routes/leaveRoute.js
@@ -17,13 +17,15 @@ const authorizeDeptHead = require('../middlewares/authorizeDeptHead');
 const authorizeRole = require('../middlewares/authorizeRole');
 
 // Giả sử HR_ROLE_ID là ID của vai trò HR trong bảng roles (thay bằng giá trị thực tế)
+const DIRECTOR_ROLE_ID = 1;
 const HR_ROLE_ID = 2; // Ví dụ, thay bằng ID thực từ model `roles`
 router.use(authenticateToken);
 
 
 // Tạo leave request (nhân viên bất kỳ)
 router.post('/',
-    
+
+    authorizeRole([DIRECTOR_ROLE_ID], [HR_ROLE_ID]),
     createLeaveRequest
 );
 
@@ -37,16 +39,31 @@ router.put('/:leaveRequestID/approve-by-dept',
 // HR duyệt hoặc từ chối leave request
 router.put('/:leaveRequestID/approve-by-hr',
     authenticateToken,
-    authorizeRole([HR_ROLE_ID]),
+    authorizeRole([HR_ROLE_ID], [DIRECTOR_ROLE_ID]),
     approveByHR
 );
 
-router.get('/', getAllLeaveRequests);
+router.get('/',
+    authorizeRole([HR_ROLE_ID], [DIRECTOR_ROLE_ID]),
+    getAllLeaveRequests
+);
 
-router.get('/pending', getAllPendingLeaveRequests);
-router.get('/rejected', getAllRejectedLeaveRequests);
-router.get('/accepted', getAllAcceptedLeaveRequests);
-router.get('/accepted-by-dept', getAllAcceptedRequestsByDept);
+router.get('/pending',
+    authorizeRole([HR_ROLE_ID], [DIRECTOR_ROLE_ID]),
+    getAllPendingLeaveRequests
+);
+router.get('/rejected',
+    authorizeRole([HR_ROLE_ID], [DIRECTOR_ROLE_ID]),
+    getAllRejectedLeaveRequests
+);
+router.get('/accepted',
+    authorizeRole([HR_ROLE_ID], [DIRECTOR_ROLE_ID]),
+    getAllAcceptedLeaveRequests
+);
+router.get('/accepted-by-dept',
+    authorizeRole([HR_ROLE_ID], [DIRECTOR_ROLE_ID]),
+    getAllAcceptedRequestsByDept
+);
 
 
 module.exports = router;
