688d61aa9c2d624dff78159c054dccb20c1fca32
Anh Hieu <thaiphananhhieu2004@gmail.com>
2025-04-02 01:44:06 +0700

    update
    add homepage + recruit portal

---
+++ Project/frontend_antd/src/app/homepage/page.tsx
@@ -0,0 +1,11 @@
+/* frontend/src/app/login/page.tsx */
+
+import RecruitmentPortal from './HomePage';
+
+export default function HomePage() {
+  return (
+    <div>
+      <RecruitmentPortal />
+    </div>
+  );
+}
